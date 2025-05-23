e3662a3b74eca3c642b2e863b574d5b34d4823ca
Aadil Mugal <aadilshe786@gmail.com>
2025-05-23 11:05:35 +0530

    feat

--- tests/app.test.js
+++
@@ -1,9 +0,0 @@
-const extractDetails = require('../src/js/app'); // Adjust path if needed
-
-test('Extract details from a valid URL', () => {
-  const url =
-    'https://marketplace.visualstudio.com/items?itemName=ms-python.python';
-  const { publisher, extension } = extractDetails(url);
-  expect(publisher).toBe('ms-python');
-  expect(extension).toBe('python');
-});
